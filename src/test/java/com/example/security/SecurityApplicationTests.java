99ba00d1e1948fa24ea77b5b39d7df26379dcdbd
ChildLee <471227142@qq.com>
2018-01-10 19:10:25 +0800

    添加redis

--- src/test/java/com/example/security/SecurityApplicationTests.java
+++ src/test/java/com/example/security/SecurityApplicationTests.java
@@ -3,6 +3,7 @@ package com.example.security;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
@@ -11,6 +12,6 @@ public class SecurityApplicationTests {
 
     @Test
     public void contextLoads() {
-
+        throw new UsernameNotFoundException("用户名：" + 1 + "不存在！");
     }
 }
