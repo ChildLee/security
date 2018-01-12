80999f32875953e1e75ac136a6cb32de8232e40a
ChildLee <471227142@qq.com>
2018-01-12 23:44:37 +0800

    commit

--- src/test/java/com/example/security/SecurityApplicationTests.java
+++ src/test/java/com/example/security/SecurityApplicationTests.java
@@ -3,7 +3,6 @@ package com.example.security;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
@@ -12,6 +11,6 @@ public class SecurityApplicationTests {
 
     @Test
     public void contextLoads() {
-        throw new UsernameNotFoundException("用户名：" + 1 + "不存在！");
+
     }
 }
